e3a92bbdf70812ec3c7481d46270efaacdb3cde1
jman13378 <garconiamail@gmail.com>
2024-01-25 16:50:50 -0500

    e

---
+++ frontend/vue/views/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+<!-- inspo https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.engadget.com%2F2019-07-10-tinder-lite.html&psig=AOvVaw2cM7oN4cRWUsggWA9_oCdO&ust=1706303540882000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNjw55m6-YMDFQAAAAAdAAAAABAh -->
+<body>
+    <div class="container">
+      <ul>
+        <li></li><!--top bar -->
+        <li></li>
+        <li><!--3 -->
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+  
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </body>
+</template>  
