037e35a14ae8b58a9b696c9580d75b5a0d92992c
jman13378 <garconiamail@gmail.com>
2024-02-15 21:11:41 -0500

    e

--- frontend/vue/App.vue
+++ frontend/vue/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="nav">
         <router-link to="/">Chat</router-link> |
-        <router-link to="/mf">Selection</router-link>
+        <router-link to="/mf">Selection</router-link>|
+        <router-link to="/chat">Chat</router-link>
+      
     </div>
     <router-view />
   </template>
