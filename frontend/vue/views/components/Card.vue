6c0049505c4a546a387d348490f70513770b9685
jman13378 <garconiamail@gmail.com>
2024-02-16 22:12:02 -0500

    e

--- frontend/vue/views/components/Card.vue
+++ frontend/vue/views/components/Card.vue
@@ -1,14 +1,19 @@
 <template>
-    <div class="card" :id='"card" +id' :style='(visible ? "" : "display:none;")'>
-        <img alt="Dog 1" :src="dogimage" />
-        <h2>{{ name }}</h2>
-        <p>Age: {{age }} Years</p>
-        <p>Breed: {{ breed }}</p>
-        <button onclick="swipe('like')" ondragover="swipemobile('like')">Like</button>
-        <button onclick="swipe('dislike')" ondragover="swipemobile('dislike')">Dislike</button>
-      </div>
+  <div class="card" :id='"card" + id' :style='(visible ? "" : "display:none;")'>
+    <img alt="Dog 1" :src="dogimage" />
+    <h2>{{ name }}</h2>
+    <p>Age: {{ age }} Years</p>
+    <p>Breed: {{ breed }}</p>
+    <button onclick="swipe('like')" :ondragover="swipemobile('like', id)">Like</button>
+    <button onclick="swipe('dislike')" :ondragover="swipemobile('dislike', id)">Dislike</button>
+  </div>
 </template>
 <script>
+window.stuffs = {
+  currentCardIndex: 1,
+  dislikeCount: 0,
+  likeCount: 0
+}
 export default {
   data() {
     return { isTargetVisible: false };
@@ -16,11 +21,11 @@ export default {
   methods: {
     swipe(action) {
       if (action === 'dislike') {
-        dislikeCount++;
+        window.stuffs.dislikeCount++;
       } else if (action === 'like') {
-        likeCount++;
+        window.stuffs.likeCount++;
 
-        if (likeCount === 3) {
+        if (likeCount === Math.floor(Math.random() * 20)) {
           window.location.href = 'Chat.html';
           return;
         }
@@ -30,9 +35,9 @@ export default {
     },
 
     showNextCard() {
+      let currentCardIndex = window.stuffs.currentCardIndex;
       const currentCard = document.getElementById(`card${currentCardIndex}`);
-      currentCardIndex++;
-
+      currentCardIndex++
       const nextCard = document.getElementById(`card${currentCardIndex}`);
       if (nextCard) {
         currentCard.style.display = 'none';
@@ -46,13 +51,13 @@ export default {
   name: "Card",
   props: {
     visible: Boolean,
-    id:Number,
+    id: Number,
     name: String,
     age: Number,
     breed: String,
-    dogimage:String,
+    dogimage: String,
 
-    
+
   },
 };
 
