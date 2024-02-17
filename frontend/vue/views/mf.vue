b27e25ea9ab136743b1c9dd259e8f08f5e6b2716
jman13378 <garconiamail@gmail.com>
2024-02-16 21:48:32 -0500

    e

--- frontend/vue/views/mf.vue
+++ frontend/vue/views/mf.vue
@@ -1,69 +1,57 @@
-
-  <template>
-    <div class="container">
-      <div id="card-container">
-        <Suspense>
-              <card v-for="i in amount" :visible="(i==1 ? true : false)"  :key="i" :age="dogs[i].age" :breed="dogs[i].breed.charAt(0).toUpperCase()+ dogs[i].breed.slice(1)" :dogimage="dogs[i].image" :id="dogs[i].id" :name="dogs[i].name" ></card>
+<template>
+  <div class="container" v-if="dogs.length > 0">
+    <div id="card-container">
+      <Suspense>
+        <card v-for="dog in dogs" :key="dog.id" :visible="true" :age="dog.age" :breed="dog.breed.charAt(0).toUpperCase()+ dog.breed.slice(1)" :dogimage="dog.image" :id="dog.id" :name="dog.name"></card>
         <template #fallback>
           <p>Loading Image...</p>
-          </template>
-            </Suspense>
-            </div>
+        </template>
+      </Suspense>
     </div>
-  </template>
+  </div>
+</template>
 
-  <script>
-  // import { CredentialManager } from "../../globals";
-  import Card from "./components/Card.vue";
-  import {dogbreeds,dognames} from  "./helper.js";
+<script>
+import Card from "./components/Card.vue";
+import { dogbreeds, dognames } from "./helper.js";
 
-  export default {
-    data() {
-      return {
-          amount: 1, //Math.floor(Math.random() * 5) + 1,
-          dogs: []
+export default {
+  data() {
+    return {
+      amount: 1, // Math.floor(Math.random() * 5) + 1,
+      dogs: []
+    };
+  },
+  methods: {
+    async getImage(breed) {
+      const response = await fetch("https://dog.ceo/api/breed/" + breed + "/images/random");
+      const json = await response.json();
+      return json.message;
+    },
+    async getAge() {
+      return Math.floor(Math.random() * 13) + 1;
+    },
+    async getName() {
+      return dognames[Math.floor(Math.random() * dognames.length)];
+    },
+    async getBreed() {
+      return dogbreeds[Math.floor(Math.random() * dogbreeds.length)];
+    }
+  },
+  async beforeMount() {
+    for (let i = 0; i < this.amount; i++) {
+      const data = {
+        id: i,
+        age: await this.getAge(),
+        breed: await this.getBreed(),
+        name: await this.getName(),
+        image: await this.getImage(data.breed) // Use await here
       };
-    },
-    methods: {
-      async getImage(breed) {
-          await fetch("https://dog.ceo/api/breed/"+breed+"/images/random")
-            .then((data) => {
-              let json = JSON.stringify(data);
-  console.log(json)
-  return json.message;
-          });
-      },
-      async getAge() {
-        return await Math.floor(Math.random() * 13) + 1;
-      },
-      async getName() {
-        return await dognames[Math.floor(Math.random() * dognames.length)];
-      },
-      async getBreed() {
-        return await dogbreeds[Math.floor(Math.random() * dogbreeds.length)];
-      },
-      // async submit() {
-      //   CredentialManager.AddCheck(this.form.username, this.form.password, undefined, (valid) => {
-      //     this.$router.push("/chat");
-      //   });
-      // },
-    },
-    
-    async beforeMount() {
-      for (let i = 0; i < this.amount; i++) {
-        let data = {
-          id: i,
-          age: await getAge(),
-          breed: await getBreed(),
-          name: await getName(),
-          image: await getImage(data.breed),
-        };
-        
-          this.dogs.push(data);
-      }
-    },
-    components: {
-      Card,
-    },
-  };
-  </script>
+      this.dogs.push(data);
+    }
+  },
+  components: {
+    Card
+  }
+};
+</script>
