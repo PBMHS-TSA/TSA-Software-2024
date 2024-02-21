9ea0fc26bb6bdc7c82fa1cb34e8ab8be9aa129e1
jman13378 <garconiamail@gmail.com>
2024-02-20 22:31:32 -0500

    e

--- frontend/src/vue/App.vue
+++ frontend/src/vue/App.vue
@@ -1,25 +1,32 @@
 <template>
-    <div id="nav">
-        <router-link to="/">Chat</router-link> |
-        <router-link to="/mf">Selection</router-link>| 
-        <router-link to="/chat">Chat</router-link>| 
-      <router-link to="/sessionid">Session</router-link>
+  <nav style="margin-bottom: 1%">
+    <div class="nav-wrapper indigo">
+      <img style="height: auto; width: auto; max-height: -webkit-fill-available" src="https://tsaweb.org/images/default-source/competitions-programs/program-logos/tsa-logoff2c085a4556450589a0a1a426c8e98f.png?sfvrsn=63b79446_0" class="center brand-logo" />
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/chat">Profile</router-link></li>
+        <li><router-link to="/chat">Dashboard</router-link></li>
+
+        <li><router-link to="/chats">Messages</router-link></li>
+        <li><a :onclick="logout">Logout</a></li>
+      </ul>
     </div>
-    <router-view />
-  </template>
-  <script>
-  import { socket, CredentialManager } from "../globals";
-  
-  export default {
-    async mounted() {
-      // if (await CredentialManager.Validate()) {
-      //   this.$router.push("/chat")
-      // } else {
-      //   this.$router.push("/login")
-      // }
-    },
-    methods: {
-      tomato() {},
-    },
-  };
-  </script>
+  </nav>
+  <router-view />
+</template>
+<script>
+import { socket, CredentialManager } from "../globals";
+
+export default {
+  async mounted() {
+    // if (await CredentialManager.Validate()) {
+    //   this.$router.push("/chat")
+    // } else {
+    //   this.$router.push("/login")
+    // }
+  },
+  methods: {
+    logout() {window.location.href= `http://localhost/socket/logout/${socket.id}`},
+  },
+};
+</script>
