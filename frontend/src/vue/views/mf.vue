40bdb11653323a1a75e24f4126ceeb422d5cb449
jman13378 <garconiamail@gmail.com>
2024-02-22 18:12:25 -0500

    e

--- frontend/src/vue/views/mf.vue
+++ frontend/src/vue/views/mf.vue
@@ -9,7 +9,7 @@
 <script>
 import OpenAI from "openai";
 
-const openai = new OpenAI({apiKey: "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"});
+const openai = new OpenAI({apiKey: "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",dangerouslyAllowBrowser:true});
 
 
 import Card from "./components/Card.vue";
