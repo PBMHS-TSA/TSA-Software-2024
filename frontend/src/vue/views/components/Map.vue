134a02dec9ba178c9c66b95faf4600514ebd09ee
jman13378 <garconiamail@gmail.com>
2024-03-19 09:02:53 -0400

    E

--- frontend/src/vue/views/components/Map.vue
+++ frontend/src/vue/views/components/Map.vue
@@ -1,125 +1,101 @@
-
 <template>
-    <div id="map">
-        
-    <!--In the following div the HERE Map will render-->
-      <div id="mapContainer" style="height:600px;width:100%" ref="hereMap"></div>
-    </div>
-    <!--<table>
-        <thead>
-          <tr>
-              <th>Name</th>
-              <th>Item Name</th>
-          </tr>
-        </thead>
+  <div id="map">
+    <!-- In the following div the HERE Map will render -->
+    <div id="mapContainer" style="height: 600px; width: 100%" ref="hereMap"></div>
+  </div>
+</template>
 
-        <tbody>
-          <tr>
-            <td><a href="https://stores.petco.com/fl/orlando/pet-supplies-orlando-fl-1748.html" target="_blank">Petco<br style="font-size:small">5151 Vineland Rd, Orlando, Florida</a></td>
-            <td>0.8 Miles</td>
-          </tr>
-          <tr>
-            <td><a href="https://stores.petco.com/fl/orlando/pet-supplies-orlando-fl-1748.html" target="_blank">Petco<br style="font-size:small">5151 Vineland Rd, Orlando, Florida</a></td>
-            <td>0.8 Miles</td>
-          </tr>
-          <tr>
-            <td><a href="https://stores.petco.com/fl/orlando/pet-supplies-orlando-fl-1748.html" target="_blank">Petco<br style="font-size:small">5151 Vineland Rd, Orlando, Florida</a></td>
-            <td>0.8 Miles</td>
-          </tr>
-        </tbody>
-      </table>-->
-  </template>
-  
-  <script>
-  export default {
-    name: "Map",
-    props: {
-      center: Object
-      // center object { lat: 40.730610, lng: -73.935242 }
-    },
-    data() {
-      return {
-        platform: null,
-        apikey: "j0QxHumNVhmcu8MHfAh8Ag-HSn4cZ4z4NScpNELwce0"
-        // You can get the API KEY from developer.here.com
-      };
-    },
+<script>
+export default {
+  name: "Map",
+  props: {
+    center: Object,
+    // center object { lat: 40.730610, lng: -73.935242 }
+  },
+  data() {
+    return {
+      platform: null,
+      apikey: "j0QxHumNVhmcu8MHfAh8Ag-HSn4cZ4z4NScpNELwce0",
+      // You can get the API KEY from developer.here.com
+    };
+  },
+  async mounted() {
+    // Initialize the platform object:
+    const platform = new window.H.service.Platform({
+      apikey: this.apikey
+    });
+    this.platform = platform;
+    this.initializeHereMap();
+  },
+  methods: {
+    async initializeHereMap() {
+      const mapContainer = this.$refs.hereMap;
+      const H = window.H;
+
+      // Obtain the default map types from the platform object
+      const maptypes = this.platform.createDefaultLayers();
+      if ("geolocation" in navigator) {
+  // Request the current position
+  navigator.geolocation.getCurrentPosition(function(position) {
+    // Retrieve latitude and longitude from the position object
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
     
-    async mounted() {
-      // Initialize the platform object:
-      const platform = new window.H.service.Platform({
-        apikey: this.apikey
+    // Do something with the latitude and longitude, such as displaying them on the webpage
+    console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+  }, function(error) {
+    // Handle errors, such as the user denying permission or if the browser does not support geolocation
+    console.error("Error getting the current position:", error);
+  });
+} else {
+  // Geolocation is not supported by the browser
+  console.error("Geolocation is not supported by this browser.");
+}
+      // Instantiate (and display) a map object:
+      const map = new H.Map(mapContainer, maptypes.vector.normal.map, {
+        zoom: 5,
+        center: { lat: latitude, lng: longitude }
       });
-      this.platform = platform;
-      this.initializeHereMap();
-    },
-    methods: {
-createMarker(lat,lng,icon, map,size) {
-    const marker = new H.map.Marker({lat: lat, lng: lng});
-        let pngIcon = new H.map.Icon(icon, {size: {w: size.width, h: size.height}});      
 
-  marker.setIcon(pngIcon)
-  map.addObject(marker);
-},
-      initializeHereMap() { // rendering map
-  
-        const mapContainer = this.$refs.hereMap;
-        const H = window.H;
-        // Obtain the default map types from the platform object
-        var maptypes = this.platform.createDefaultLayers();
-  
-        // Instantiate (and display) a map object:
-        var map = new H.Map(mapContainer, maptypes.vector.normal.map, {
-          zoom: 10,
-          center: {lng:-81.4566075,lat:28.4820108}
-          // center object { lat: 40.730610, lng: -73.935242 }
-        });
-  
-        addEventListener("resize", () => map.getViewPort().resize());
-  
-        // add behavior control
-        new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-  
-        // add UI
-        H.ui.UI.createDefault(map, maptypes);
-        let pins = {
-            location:"https://i.ibb.co/Fh7GNsy/location-location-pin-location-icon-transparent-free-png.png",
-            shops: "https://i.ibb.co/1vXMzNK/tom-fotor-bg-remover-20240222215654.png",
-            groomer: "https://i.ibb.co/N3S6xQP/Groomer-pin-removebg-preview.png",
-            dogwalkers: "https://i.ibb.co/PYFC9M3/walking-pin-removebg-preview.png",
-            parks: "https://i.ibb.co/0c90tpD/Untitled-design-1-removebg-preview.png",  
-        }
-        window.maperer = map
+      addEventListener("resize", () => map.getViewPort().resize());
 
-        // End rendering the initial map28.49242972397546, -81.46890825104127
-        this.createMarker(28.4820108,-81.4566075,pins.location, map, {width: 56, height: 56})
-        this.createMarker(28.49242972397546, -81.46890825104127,pins.shops, map, {width: 56, height: 56})
-        this.createMarker(28.496303761987644, -81.42943979263434,pins.shops, map, {width: 56, height: 56})
-        this.createMarker(28.51466439300104, -81.48442262819522,pins.shops, map, {width: 56, height: 56})
-        this.createMarker(28.494163431126942, -81.46982592908185,pins.groomer, map, {width: 56, height: 56})
-        this.createMarker(28.507509646254267, -81.45729211781445,pins.groomer, map, {width: 56, height: 56})
-        this.createMarker(28.4846335635329, -81.4609175293766,pins.dogwalkers, map, {width: 56, height: 56})
-        this.createMarker(28.487033485843625, -81.46200114175743,pins.dogwalkers, map, {width: 56, height: 56})
-        this.createMarker(28.486646861578627, -81.46393769659025,pins.dogwalkers, map, {width: 56, height: 56})
-        this.createMarker(28.48462044269299, -81.46526029961568,pins.dogwalkers, map, {width: 56, height: 56})
-        this.createMarker(28.483722805516027, -81.465062045014,pins.dogwalkers, map, {width: 56, height: 56})
-        this.createMarker(28.489545034717654, -81.48231208709925,pins.parks, map, {width: 56, height: 56})
-        this.createMarker(28.492562469524763, -81.47819221421392,pins.parks, map, {width: 56, height: 56})
-        this.createMarker(28.514888805984743, -81.43441856480727,pins.parks, map, {width: 56, height: 56})
+      // Add behavior control
+      new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
-        // Add marker to the map
+      // Add UI
+      H.ui.UI.createDefault(map, maptypes);
+
+      // Fetch points of interest using HERE Places API
+      const response = await fetch(
+        `https://places.ls.hereapi.com/places/v1/discover/around?at=28.4820108,-81.4566075&apiKey=${this.apikey}`
+      );
+
+      if (!response.ok) {
+        console.error("Failed to fetch POIs");
+        return;
       }
+
+      const data = await response.json();
+
+      // Display POIs on the map
+      data.results.items.forEach(place => {
+        const marker = new H.map.Marker({
+          lat: place.position[0],
+          lng: place.position[1]
+        });
+        map.addObject(marker);
+      });
     }
-  };
-  </script>
-  
-  <style scoped>
-  #map {
-    width: 60vw;
-    min-width: 360px;
-    text-align: center;
-    margin: 5% auto;
-    background-color: #ccc;
   }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+#map {
+  width: 60vw;
+  min-width: 360px;
+  text-align: center;
+  margin: 5% auto;
+  background-color: #ccc;
+}
+</style>
