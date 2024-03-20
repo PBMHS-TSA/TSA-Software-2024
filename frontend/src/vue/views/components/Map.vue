b7577fa5c8b0c83666f549bf344d3045b22eb7bc
jman13378 <garconiamail@gmail.com>
2024-03-20 08:53:40 -0400

    E

--- frontend/src/vue/views/components/Map.vue
+++ frontend/src/vue/views/components/Map.vue
@@ -20,52 +20,51 @@ export default {
     };
   },
   async mounted() {
-    // Initialize the platform object:
-    const platform = new window.H.service.Platform({
-      apikey: this.apikey
-    });
-    this.platform = platform;
     if ("geolocation" in navigator) {
-  // Request the current position
-   navigator.geolocation.getCurrentPosition( (position) => {
-    // Retrieve latitude and longitude from the position object
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    this.initializeHereMap(latitude,longitude);
-
-    // Do something with the latitude and longitude, such as displaying them on the webpage
-    console.log("Latitude: " + latitude + ", Longitude: " + longitude);
-  }, function(error) {
-    // Handle errors, such as the user denying permission or if the browser does not support geolocation
-    console.error("Error getting the current position:", error);
-  });
-} else {
-  // Geolocation is not supported by the browser
-  console.error("Geolocation is not supported by this browser.");
-}
-
+      // Request the current position
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // Retrieve latitude and longitude from the position object
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          this.initializeHereMap(latitude, longitude);
+          // Do something with the latitude and longitude, such as displaying them on the webpage
+          console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+        },
+        function(error) {
+          // Handle errors, such as the user denying permission or if the browser does not support geolocation
+          console.error("Error getting the current position:", error);
+        }
+      );
+    } else {
+      // Geolocation is not supported by the browser
+      console.error("Geolocation is not supported by this browser.");
+    }
   },
   methods: {
-    createMarker(lat,lng,icon, map,size) {
-    const marker = new H.map.Marker({lat: lat, lng: lng});
-        let pngIcon = new H.map.Icon(icon, {size: {w: size.width, h: size.height}});      
-
-  marker.setIcon(pngIcon)
-  map.addObject(marker);
-},
-    async initializeHereMap(lat,lng) {
+    createMarker(lat, lng, icon, map, size) {
+      const marker = new H.map.Marker({ lat: lat, lng: lng });
+      const pngIcon = new H.map.Icon(icon, { size: { w: size.width, h: size.height } });
+      marker.setIcon(pngIcon);
+      map.addObject(marker);
+    },
+    async initializeHereMap(lat, lng) {
       const mapContainer = this.$refs.hereMap;
       const H = window.H;
 
+      // Initialize the platform object:
+      this.platform = new H.service.Platform({
+        apikey: this.apikey
+      });
+
       // Obtain the default map types from the platform object
       const maptypes = this.platform.createDefaultLayers();
-      
+
       // Instantiate (and display) a map object:
       const map = new H.Map(mapContainer, maptypes.vector.normal.map, {
         zoom: 10,
         center: { lat: lat, lng: lng }
       });
-      
 
       addEventListener("resize", () => map.getViewPort().resize());
 
@@ -74,43 +73,31 @@ export default {
 
       // Add UI
       H.ui.UI.createDefault(map, maptypes);
-      let pins = {
-            location:"https://i.ibb.co/Fh7GNsy/location-location-pin-location-icon-transparent-free-png.png",
-            shops: "https://i.ibb.co/1vXMzNK/tom-fotor-bg-remover-20240222215654.png",
-            groomer: "https://i.ibb.co/N3S6xQP/Groomer-pin-removebg-preview.png",
-            dogwalkers: "https://i.ibb.co/PYFC9M3/walking-pin-removebg-preview.png",
-            parks: "https://i.ibb.co/0c90tpD/Untitled-design-1-removebg-preview.png",  
-        }
 
-      this.createMarker(lat, lng,pins.location, map, {width: 56, height: 56})
-      // Fetch points of interest using HERE Places API
-      const response = await fetch(
-        `https://cors-anywhere.herokuapp.com/https://places.ls.hereapi.com/places/v1/discover/around?at=28.4820108,-81.4566075&apiKey=${this.apikey}`,
-        {
-          headers: {
-            "X-Requested-With": "XMLHttpRequest"
-          }
-        }
-      );
+      // Fetch points of interest using HERE Geocoding & Search API v7
+      const categories = {
+        parks: "dog-park",
+        groomers: "dog-groomer",
+        petStores: "pet-store"
+      };
 
-      if (!response.ok) {
-        console.error("Failed to fetch POIs");
-        return;
-      }
-
-      const data = await response.json();
-
-      // Display POIs on the map
-      data.results.items.forEach(place => {
-        console.log(place)
-        const marker = new H.map.Marker({
-          lat: place.position[0],
-          lng: place.position[1]
+      for (const category of Object.keys(categories)) {
+        const places = await this.fetchDogCarePlaces(category, lat, lng);
+        places.forEach(place => {
+          this.createMarker(place.position[0], place.position[1], pins[category], map, { width: 56, height: 56 });
         });
-        map.addObject(marker);
-      });
-      window.maperer = map
-
+      }
+    },
+    async fetchDogCarePlaces(category, lat, lng) {
+      const url = `https://discover.search.hereapi.com/v1/discover?at=${lat},${lng}&q=${category}&limit=10&apiKey=${this.apikey}`;
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data.items;
+      } catch (error) {
+        console.error(`Error fetching ${category}:`, error);
+        return [];
+      }
     }
   }
 };
