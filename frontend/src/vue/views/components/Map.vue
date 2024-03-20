a2c1f3e9d9ab358c3aed544f763bd433bc69a958
jman13378 <garconiamail@gmail.com>
2024-03-20 08:56:21 -0400

    E

--- frontend/src/vue/views/components/Map.vue
+++ frontend/src/vue/views/components/Map.vue
@@ -73,7 +73,16 @@ export default {
 
       // Add UI
       H.ui.UI.createDefault(map, maptypes);
+      window.maperer = map;
+      let pins = {
+            location:"https://i.ibb.co/Fh7GNsy/location-location-pin-location-icon-transparent-free-png.png",
+            shops: "https://i.ibb.co/1vXMzNK/tom-fotor-bg-remover-20240222215654.png",
+            groomer: "https://i.ibb.co/N3S6xQP/Groomer-pin-removebg-preview.png",
+            dogwalkers: "https://i.ibb.co/PYFC9M3/walking-pin-removebg-preview.png",
+            parks: "https://i.ibb.co/0c90tpD/Untitled-design-1-removebg-preview.png",  
+        }
 
+      this.createMarker(lat, lng,pins.location, map, {width: 56, height: 56})
       // Fetch points of interest using HERE Geocoding & Search API v7
       const categories = {
         parks: "dog-park",
