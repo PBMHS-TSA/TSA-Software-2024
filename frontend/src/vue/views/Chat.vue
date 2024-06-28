f08227cf1d6c756bbc48fc4aa022d10feb641197
jman13378 <garconiamail@gmail.com>
2024-06-28 12:53:20 -0400

    e

--- frontend/src/vue/views/Chat.vue
+++ frontend/src/vue/views/Chat.vue
@@ -1,62 +1,58 @@
 <template>
   <Map id="map-selector" class="disabled"></Map>
-  <div class="container" style="align-items: unset;">
+  <div class="container" style="align-items: unset">
     <h2>Chat with Marley</h2>
     <div id="chat-messages">
-      <div class="message received">
+      <div class="message received" v-if="id != 1">
         <p>Hello! How are you?</p>
       </div>
-      <div class="message sent">
+      <div class="message sent" v-if="id != 1">
         <p>Hi! I'm doing great, thanks!</p>
       </div>
-      <div class="message received">Thats Good! Where do you want to meet up?</div>
+      <div class="message received" v-if="id != 1">Thats Good! Where do you want to meet up?</div>
     </div>
-    <div class="row" style="margin-top:5%">
+    <div class="row" style="margin-top: 5%">
       <div class="input-field col s8">
         <input placeholder="" id="message-input" type="text" class="validate" />
         <label for="message-input">Type your message...</label>
       </div>
       <div class="input-field col s2">
         <a class="waves-effect waves-light btn" :onclick="sendMessage"><i class="material-icons">send</i></a>
-
-      </div>      <div class="input-field col s2">
+      </div>
+      <div class="input-field col s2">
         <a class="waves-effect waves-light btn location-selector" :onclick="createMap"><i class="material-icons">public</i></a>
-
-      </div>  
+      </div>
     </div>
   </div>
 </template>
 <style>
 #map-selector.disabled {
-display:none !important;
+  display: none !important;
 }
 .map-selector {
-  position:absolute;
+  position: absolute;
 }
 .location-selector {
-
 }
 .message.sent {
-margin-left:50%;
-
+  margin-left: 50%;
 }
 .message.received {
- margin-right:50%;
+  margin-right: 50%;
 }
 .message {
   min-width: 100px;
-  min-height:100px;
+  min-height: 100px;
   max-width: 200px;
-  max-height:200px;
-  
+  max-height: 200px;
 }
 </style>
 <script>
-import Map from "./components/Map.vue"
+import Map from "./components/Map.vue";
 export default {
   methods: {
     createMap() {
-document.getElementById("map-selector").classList.toggle("disabled")
+      document.getElementById("map-selector").classList.toggle("disabled");
     },
     getRandomResponse(index) {
       const responses = ["Hello!", "Good! How about you?", "Thats Good! Where do you want to meet up?"];
@@ -91,7 +87,7 @@ document.getElementById("map-selector").classList.toggle("disabled")
     },
   },
   components: {
-    Map
-  }
+    Map,
+  },
 };
 </script>
