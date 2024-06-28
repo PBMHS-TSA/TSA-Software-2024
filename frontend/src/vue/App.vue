aa8ed062288ce87bbd9d25253f82f75353d673eb
jman13378 <garconiamail@gmail.com>
2024-06-28 08:34:29 -0400

    e

--- frontend/src/vue/App.vue
+++ frontend/src/vue/App.vue
@@ -2,7 +2,10 @@
   <nav style="position: fixed ; margin-bottom: 1%; z-index: 100; top: 0px">
     <div class="nav-wrapper indigo">
       <img style="height: auto; width: auto; max-height: -webkit-fill-available" src="https://firebasestorage.googleapis.com/v0/b/twetzel-a07f2.appspot.com/o/logo.png?alt=media&token=03163384-242a-4530-9d14-3409521b721a" class="center brand-logo" />
-      <ul id="nav-mobile" class="left hide-on-med-and-down"></ul>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+<li><router-link to="/mobileview">Mobile View</router-link></li>
+
+      </ul>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/profile">Profile</router-link></li>
